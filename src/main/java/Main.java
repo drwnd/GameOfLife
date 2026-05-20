09813a4c93d85a8d261097f9adb806b737ab8724
drwnd <markus.vonhaenisch@gmail.com>
2026-05-20 11:45:05 +0200

    downgraded Java version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import core.rendering_api.Window;
 import core.settings.Settings;
 import game.Renderer;
 
-void main() {
-    Settings.loadFromFile();
-    Window.init("Game Of Life");
-    Window.pushRenderable(new Renderer());
-    Window.renderLoop();
-    Window.cleanUp();
+public final class Main {
+    public static void main(String[] args) {
+        Settings.loadFromFile();
+        Window.init("Game Of Life");
+        Window.pushRenderable(new Renderer());
+        Window.renderLoop();
+        Window.cleanUp();
+    }
 }
